0a7d25416dc63df9e079e0f94c796cac0decc4a7
PeanutKoala <91756807+PeanutKoala@users.noreply.github.com>
2022-03-08 20:46:17 -0600

    Bee Patrolling Scene
    Still need to fix patrolling bee orientation.
    Added new assets (low poly tree and nature packs).

---
+++ ToBeeOrNotToBee/Assets/Scripts/Patroller.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patroller : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int speed;
+
+    public int waypointIndex;
+    private float dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+        if(dist < 0.05f)
+        {
+            IncreaseIndex();
+        }
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime / 10);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if(waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+}
