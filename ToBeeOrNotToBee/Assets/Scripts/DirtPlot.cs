b204d89d6968d24370bca9baa3c74257afeb9576
Jonathan Konderla <JonathanKonderla@gmail.com>
2022-03-21 16:02:52 -0500

    plow and plant update

--- ToBeeOrNotToBee/Assets/Scripts/DirtPlot.cs
+++ ToBeeOrNotToBee/Assets/Scripts/DirtPlot.cs
@@ -4,47 +4,28 @@ using UnityEngine;
 
 public class DirtPlot : MonoBehaviour
 {
-    private enum PlotState
-    {
-        Empty,
-        Growing,
-        Pick
-    }
+    private bool isEmpty = true;
+    private bool isPlowed = false;
 
-    private PlotState state;
+    public Material unplowedMat;
+    public Material plowedMat;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        state = PlotState.Empty;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        switch (state)
-        {
-            case PlotState.Empty:
-                break;
-            case PlotState.Growing:
-                break;
-            case PlotState.Pick:
-                break;
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         print("trigger");
-        if(state == PlotState.Empty && other.gameObject.CompareTag("Seed"))
+        if(isEmpty && isPlowed && other.gameObject.CompareTag("Seed"))
         {
             GameObject plant = other.gameObject.transform.parent.gameObject;
-            Destroy(other.gameObject);
-            plant.GetComponent<Plant>().state = PlantState.Stem;
-            plant.GetComponent<Rigidbody>().isKinematic = true;
+            plant.GetComponent<Plant>().PlantSeed();
             plant.transform.position = this.transform.position;
-            plant.transform.Find("Stem").gameObject.SetActive(true);
-            state = PlotState.Growing;
+            plant.transform.rotation = Quaternion.identity;
+            isEmpty = false;
+        }
+        else if (other.gameObject.CompareTag("Plow"))
+        {
+            isPlowed = true;
+            GetComponent<MeshRenderer>().material = plowedMat;
         }
     }
 }
--- ToBeeOrNotToBee/Assets/Scripts/Plant.cs
+++ ToBeeOrNotToBee/Assets/Scripts/Plant.cs
@@ -11,11 +11,22 @@ public enum PlantState
 
 public class Plant : MonoBehaviour
 {
+    private GameObject seed;
+    private GameObject stem;
+    private GameObject fruit;
+
     public PlantState state;
     public float growTime;
     private float remainingTime;
     public float value;
     private float quality;
+
+    private void Awake()
+    {
+        seed = gameObject.transform.GetChild(0).gameObject;
+        stem = gameObject.transform.GetChild(1).gameObject;
+        fruit = gameObject.transform.GetChild(2).gameObject;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -36,12 +47,20 @@ public class Plant : MonoBehaviour
                 if (remainingTime < 0)
                 {
                     state = PlantState.Fruit;
-                    Destroy(gameObject.transform.Find("Stem"));
-                    gameObject.transform.Find("Fruit").gameObject.SetActive(true);
+                    Destroy(stem);
+                    fruit.SetActive(true);
                 }
                 break;
             case PlantState.Fruit:
                 break;
         }
+    }
+
+    public void PlantSeed()
+    {
+        Destroy(seed);
+        state = PlantState.Stem;
+        GetComponent<Rigidbody>().isKinematic = true;
+        stem.SetActive(true);
     }
 }
