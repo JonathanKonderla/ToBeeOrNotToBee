d9f726a76b63c4896e0f9bd017026f5036738f93
PeanutKoala <91756807+PeanutKoala@users.noreply.github.com>
2022-04-19 09:06:11 -0500

    Bee Level

--- ToBeeOrNotToBee/Assets/Scripts/BeeLevel.cs
+++ ToBeeOrNotToBee/Assets/Scripts/BeeLevel.cs
@@ -6,14 +6,20 @@ using UnityEngine.UI;
 public class BeeLevel : MonoBehaviour
 {
 
-    public int bee_level;
+    public float bee_level;
     public Text bee_level_text;
+    GameObject beecube;
+    private float level_low;
+    private float level_high;
+    private float cube_y;
 
     // Start is called before the first frame update
     void Start()
     {
-        bee_level = 20;
+        bee_level = 20f;
         bee_level_text.text = "Bee Level: " + bee_level.ToString();
+        level_low = 0.92f;
+        level_high = 1.655f;
     }
 
     // Update is called once per frame
@@ -26,11 +32,16 @@ public class BeeLevel : MonoBehaviour
     {
         bee_level = bee_level + (100 - bee_level)/10;
         bee_level_text.text = "Bee Level: " + bee_level.ToString();
+        cube_y = (bee_level * (level_high - level_low) + level_low);
+        beecube.transform.position = new Vector3(0.0744f, cube_y, 0.1254f);
+        
     }
 
     public void Decrease_Bees(int decrease_bees_by)
     {
         bee_level = bee_level - decrease_bees_by;
         bee_level_text.text = "Bee Level: " + bee_level.ToString();
+        cube_y = (bee_level * (level_high - level_low) + level_low);
+        beecube.transform.position = new Vector3(0.0744f, cube_y, 0.1254f);
     }
 }
